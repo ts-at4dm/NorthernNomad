1d94f787524cc77f8b6b842210dbec251f7aeb84
terran-stone <terran.stone@maine.edu>
2024-08-25 18:26:41 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/* Line 3 - 10 is the functions for the navbar */
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+/* ------------------*/
